0a2daf97b5c34967b5934b89b47877960eb0603e
lisandrorodriguez <lisandro.rodriguez.09@hotmail.com>
2021-12-08 23:54:20 -0300

    Se creo el modelo Usuarios

--- ODS/Usuarios/models.py
+++ ODS/Usuarios/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 class Usuario(models.Model):
-    DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-    name = 'Usuario'
+    id = models.AutoField(primary_key=True)
+    nombre_usuario = models.CharField('Nombre de usuario', max_length=100, blank=False)
+    contraseña = models.CharField('Contraseña',max_length=100, blank=False)
+    rol = models.CharField('Rol de usuario', max_length=100, blank=False)
+    fecha_de_creacion = models.DateField('Fecha creacion', auto_now=False, auto_now_add=True)
+    
+    def __str__(self):
+        return self.nombre_usuario
+        
+
+
+
+
+
+
