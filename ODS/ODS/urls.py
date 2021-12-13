c46e0e4b35a8b29d5808356f4a47be5e135ac97e
lisandrorodriguez <lisandro.rodriguez.09@hotmail.com>
2021-12-13 12:56:15 -0300

    Se creo el template del login y se hizo las conecciones con css.

--- ODS/ODS/urls.py
+++ ODS/ODS/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/', views.inicio)
+    path('inicio/', views.inicio),
+    path('login/', views.login)
 ]
