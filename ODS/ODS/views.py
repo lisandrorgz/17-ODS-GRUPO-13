6debc6610ad63cc23ec451c263d1c9a53a86ff54
lisandrorodriguez <lisandro.rodriguez.09@hotmail.com>
2021-12-14 20:34:34 -0300

    Cambios en templates

--- ODS/ODS/views.py
+++ ODS/ODS/views.py
@@ -3,6 +3,5 @@ from django.shortcuts import render
 def inicio(request):
     return render(request, 'inicio.html')
 
-def login(request):
-    
+def login(request):  
     return render(request, 'login.html')
