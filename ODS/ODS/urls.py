592d8bdaee8e2161fb04763fc00f30362fb04bb3
lisandrorodriguez <lisandro.rodriguez.09@hotmail.com>
2021-12-15 17:10:16 -0300

    Se utilizo herencia de plantillas

--- ODS/ODS/urls.py
+++ ODS/ODS/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.inicio, name="inicio"),
+    path('',views.inicio, name="inicio"),
     path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
 	path('logout/', auth_views.logout_then_login, name = "logout"),
     path('foro/', views.foro, name="foro" )
