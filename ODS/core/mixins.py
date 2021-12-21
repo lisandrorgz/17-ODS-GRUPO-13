1c0d79680d2dab600243ac799485798f22054d40
jorgehara <empresamodelo2018@gmail.com>
2021-12-21 06:16:35 -0300

    hay un problema con el registro y login, una vez que registra no deja loguearse

--- ODS/core/mixins.py
+++ ODS/core/mixins.py
@@ -4,6 +4,6 @@ class AdminRequiredMixins():
     # VER CONDICIONES DE permisos_requeridos = []
         def dispatch(self, request, *args, **kwars):
         # print(self.permisos_requeridos)
-            if not request.user.is_superuser:
+            if not request.user.is_admin:
                 raise PermissionDenied
             return super(AdminRequiredMixins, self).dispatch(request, *args, **kwars)
